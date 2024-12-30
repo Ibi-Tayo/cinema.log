6bbc8ad001035f79d1046e301411357815d96c0e
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2024-12-30 02:55:54 +0000

    update

--- cinema.log.server/Abstractions/Interfaces/IUserService.cs
+++ cinema.log.server/Abstractions/Interfaces/IUserService.cs
@@ -4,9 +4,9 @@ namespace cinema.log.server.Abstractions.Interfaces;
 
 public interface IUserService
 {
-    Task<UserDto> GetUser(Guid userId);
-    Task<UserDto> AddUser(UserDto user);
-    Task<UserDto> UpdateUser(UserDto user);
+    Task<UserDto?> GetUser(Guid userId);
+    Task<Response<UserDto>> AddUser(UserDto user);
+    Task<Response<UserDto>> UpdateUser(UserDto user);
     Task<bool> DeleteUser(Guid userId);
     Task<List<ReviewDto>> GetUserReviews(Guid userId);
     Task<List<FilmDto>> GetFilmsReviewedByUser(Guid userId);
