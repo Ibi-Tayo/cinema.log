ddcb0ab43e8d67d5b77fdfc2f7291bf3d5f88012
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-01-10 00:34:30 +0000

    -

--- cinema.log.server/Repositories/Migrations/CinemaLogContextModelSnapshot.cs
+++ cinema.log.server/Repositories/Migrations/CinemaLogContextModelSnapshot.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using cinema.log.server.Models;
+using cinema.log.server.Models.Entities;
 
 #nullable disable
 
