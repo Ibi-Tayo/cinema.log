1eedf72a50da2f934b9ce8622d5c7d8f0eab57cf
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-02-05 22:10:23 +0000

    refactor: simplify controller to rely on response generated by service

--- cinema.log.server/cinema.log.server/Controllers/UserController.cs
+++ cinema.log.server/cinema.log.server/Controllers/UserController.cs
@@ -11,53 +11,20 @@ public class UserController(IUserService userService) : ControllerBase
 {
     [HttpGet]
     [Route("{userId}")]
-    public async Task<ActionResult<Response<UserDto>>> GetUser(Guid userId)
-    {
-        var resp = await userService.GetUser(userId);
-        return resp.StatusCode switch
-        {
-            StatusCodes.Status200OK => Ok(resp),
-            StatusCodes.Status404NotFound => NotFound(resp),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, resp)
-        };
-    }
-    
+    public async Task<Response<UserDto>> GetUser(Guid userId) => await userService.GetUser(userId);
+
     [HttpPost]
     [Route("AddUser")]
-    public async Task<ActionResult<Response<UserDto>>> AddUser(UserDto newUser)
-    {
-        var resp = await userService.AddUser(newUser);
-        return resp.StatusCode switch
-        {
-            StatusCodes.Status201Created => CreatedAtAction("AddUser", resp),
-            StatusCodes.Status400BadRequest => BadRequest(resp),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, resp)
-        };
-    }
-    
+    public async Task<Response<UserDto>> AddUser(UserDto newUser) => await userService.AddUser(newUser);
+
     [HttpPut]
     [Route("UpdateUser")]
-    public async Task<ActionResult<Response<UserDto>>> UpdateUser(UserDto existingUser)
-    {
-        var resp = await userService.UpdateUser(existingUser);
-        return resp.StatusCode switch
-        {
-            StatusCodes.Status200OK => Ok(resp),
-            StatusCodes.Status400BadRequest => BadRequest(resp),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, resp)
-        };
-    }
+    public async Task<ActionResult<Response<UserDto>>> UpdateUser(UserDto existingUser) 
+        => await userService.UpdateUser(existingUser);
+
 
     [HttpDelete]
     [Route("DeleteUser")]
-    public async Task<ActionResult<Response<UserDto>>> DeleteUser(Guid userId)
-    {
-        var response = await userService.DeleteUser(userId);
-        return response.StatusCode switch
-        {
-            StatusCodes.Status204NoContent => StatusCode(204, response),
-            StatusCodes.Status404NotFound => NotFound(response),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, response)
-        };
-    }
+    public async Task<ActionResult<Response<UserDto>>> DeleteUser(Guid userId) 
+        => await userService.DeleteUser(userId);
 }
