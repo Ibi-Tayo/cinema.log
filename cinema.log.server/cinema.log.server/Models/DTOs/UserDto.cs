edd3122ba14dade6da8da05a8d9cb6ace68e7c2b
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-02-08 01:46:55 +0000

    fix: JsonPropertyName for dto

--- cinema.log.server/cinema.log.server/Models/DTOs/UserDto.cs
+++ cinema.log.server/cinema.log.server/Models/DTOs/UserDto.cs
@@ -1,12 +1,17 @@
+using System.Text.Json.Serialization;
 using cinema.log.server.Models.Entities;
 
 namespace cinema.log.server.Models.DTOs;
 
 public class UserDto
 {
+    [JsonPropertyName("userId")]
     public Guid UserId { get; set; }
+    [JsonPropertyName("name")]
     public required string Name { get; set; }
+    [JsonPropertyName("username")]
     public required string Username { get; set; }
+    [JsonPropertyName("profilePicUrl")]
     public string? ProfilePicUrl { get; set; }
     
 }
