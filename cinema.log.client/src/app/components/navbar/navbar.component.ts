2e6a09336d643d9d3047b9acd979c13b47babff1
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-02-15 00:33:41 +0000

    feat: media query nav

--- cinema.log.client/src/app/components/navbar/navbar.component.ts
+++ cinema.log.client/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
