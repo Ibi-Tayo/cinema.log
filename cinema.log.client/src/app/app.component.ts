689b701ddc0a6026cb331601bf02017cb07edcf3
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-02-18 01:41:46 +0000

    feat: very basic routing set up

--- cinema.log.client/src/app/app.component.ts
+++ cinema.log.client/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  standalone: true
+  standalone: true,
 })
 export class AppComponent {
   title = 'cinema.log.client';
--- cinema.log.client/src/app/app.routes.ts
+++ cinema.log.client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ReviewComponent } from './components/review/review.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'review', component: ReviewComponent },
+  { path: 'login', component: LoginComponent }, // Consider redirecting to profile if we are authenticated
+  { path: 'profile', component: ProfileComponent },
+];
