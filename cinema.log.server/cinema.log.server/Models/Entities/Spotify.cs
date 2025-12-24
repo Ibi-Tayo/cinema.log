84f2a71d0c724a5a3d108c95158641c6a87a9c81
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2025-12-24 17:03:45 +0000

    remove old server

--- cinema.log.server/cinema.log.server/Models/Entities/Spotify.cs
+++
@@ -1,8 +0,0 @@
-namespace cinema.log.server.Models.Entities;
-
-public class Spotify
-{
-    public Guid Id { get; set; }
-    public string AccessToken { get; set; }
-    public DateTime ExpiryDate { get; set; }
-}
