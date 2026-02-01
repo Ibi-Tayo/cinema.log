85e7eb939c7939f1097354c13258014705b111db
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2026-02-01 09:50:09 +0000

    update pipeline and config

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,12 +31,12 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+
     },
 
     {
@@ -71,9 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200',
+    cwd: './cinema.log.client',
+    reuseExistingServer: !process.env.CI,
+  },
 });
