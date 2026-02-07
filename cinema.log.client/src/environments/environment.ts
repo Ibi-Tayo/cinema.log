26ed0585d35fe61e809cbdea6daabcaf5bd73486
Ibitayo <ibitayo@Ibitayos-MacBook-Air.local>
2026-02-07 03:07:49 +0000

    reset

--- cinema.log.client/src/environments/environment.ts
+++ cinema.log.client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://cinema-log.up.railway.app/api',
+  apiUrl: 'https://cinema-log.up.railway.app/api',
 };
